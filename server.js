16a8455a8fd2de507de6ce7c0b39352182002341
Krishna <tamadakrishnaa@gmail.com>
2023-11-01 20:28:46 +0530

    backend

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ if(process.env.NODE_ENV === 'production'){
         methods: ['GET','POST','DELETE']
     }));
 
-    app.get('*', (req, res)=>{
+    app.get('/', (req, res)=>{
         res.send('API')
         // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
